50092407b69c40fde6dd71caba2c6a1d9cec50d8
Alex Skreen <skreenalex@gmail.com>
2020-03-20 10:49:01 -0700

    add splash page

--- Salon/Models/Client.cs
+++ Salon/Models/Client.cs
@@ -5,10 +5,6 @@ namespace Salon.Models
 {
   public class Client
   {
-    public Client()
-    {
-      this.Reviews = new HashSet<Review>();
-    }
     public int ClientId { get; set; }
     public string Name { get; set; }
     public int StylistId { get; set; }
