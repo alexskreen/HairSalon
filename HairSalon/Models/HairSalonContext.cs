49688f92d9c1f9d615e4f698bc085e31c24fca15
Alex Skreen <skreenalex@gmail.com>
2020-03-20 17:05:38 -0700

    fix naming convention

---
+++ HairSalon/Models/HairSalonContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Models
+{
+  public class SalonContext : DbContext
+  {
+    public virtual DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    
+    public SalonContext(DbContextOptions options) : base(options) { }
+  }
+}
